0445592652323a6fdc684e1368d4c7d87969155c
kipkemoimayor <collinskipkemoi24@gmail.com>
2019-02-12 11:27:18 +0300

    This commit adds break line to document.write

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,20 @@ return num1+num2;
 function sab(num1,num2){
 return num1-num2;
 }
+function div(num1,num2){
+return num1/num2;
+}
+function mul(num1,num2){
+return num1*num2;
+}
 var n=parseFloat(prompt("enter a number"));
 var m=parseFloat(prompt("enter second value"));
 var re=add(n,m);
-document.write("Addition results is "+re +"\n");
+document.write("<b>Addition results is</b> "+re +"\n");
+document.write("<br>")
 var ss=sab(n,m)
-document.write("\n substraction re is "+ss)
+document.write("substraction re is "+ss+"<br>")
+var di=div(n,m)
+document.write("\n division results is "+di+"<br>")
+var ml=mul(n,m)
+document.write("\n <b>multiplication results is</b> "+ml)
